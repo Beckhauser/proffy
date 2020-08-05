4630b6c684709569366ee9d6614b35f746180901
Lucas Alexandre Beckhauser <38123030+Beckhauser@users.noreply.github.com>
2020-08-05 16:44:11 -0300

    Create and Stryle TeacherItem Component

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import './styles.css';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+
+function TeacherItem() {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img src="https://pbs.twimg.com/profile_images/1069690828375904263/SXs-5rMN_400x400.jpg" alt="Lucas" />
+        <div>
+          <strong>Lucas Beckhauser</strong>
+          <span>League of Legends</span>
+        </div>
+      </header>
+
+      <p>
+        Entusiasta dos melhores metas da BotLane
+            <br /><br />
+            Apaixonado por farmar a jungle do time e por feedar na botlane entegando partidas.
+          </p>
+
+      <footer>
+        <p>
+          Preço/hora
+              <strong> R$ 10,00</strong>
+        </p>
+        <button>
+          <img src={whatsappIcon} alt="Whatsapp" />
+              Entrar em contato
+            </button>
+      </footer>
+
+    </article>
+  );
+}
+
+export default TeacherItem;
