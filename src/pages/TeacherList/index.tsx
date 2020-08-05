f3bab03f927d492b927e6658af8fafe260a5069d
Lucas Alexandre Beckhauser <38123030+Beckhauser@users.noreply.github.com>
2020-08-05 16:46:23 -0300

    Create and Style TeacherList

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,8 +1,35 @@
 import React from 'react';
+import PageHeader from '../../components/PageHeader/index'
+
+import './styles.css';
+import TeacherItem from '../../components/TeacherItem';
+
 
 function TeacherList() {
   return (
-    <h1> Teachers </h1>
+    <div id="page-teacher-list" className="container">
+      <PageHeader title="Esse são os proffys disponíveis.">
+        <form id="search-teachers">
+          <div className="input-block">
+            <label htmlFor="subject">Matéria</label>
+            <input type="text" id="subject" />
+          </div>
+          <div className="input-block">
+            <label htmlFor="week-day">Dia da semana</label>
+            <input type="text" id="week-day" />
+          </div>
+          <div className="input-block">
+            <label htmlFor="time">Horário</label>
+            <input type="text" id="time" />
+          </div>
+
+        </form>
+      </PageHeader>
+
+      <main>
+        <TeacherItem />
+      </main>
+    </div>
   )
 }
 
